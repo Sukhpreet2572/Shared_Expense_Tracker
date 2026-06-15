74e86e89f28f81032ea8b02cc34d054d3ec0e7af
Sukhpreet <sukhgill2572@gmail.com>
2026-06-15 10:20:57 +0530

    Switch to SQLite for initial deployment

--- backend/settings.py
+++ backend/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shared_expense_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Sukh@321',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
