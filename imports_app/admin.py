6fa63a2cb95da13528315575d18ee5b97922c260
Sukhpreet <sukhgill2572@gmail.com>
2026-06-15 09:17:38 +0530

    Add sample group and membership test data

--- imports_app/admin.py
+++ imports_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ImportBatch, Anomaly
 
-# Register your models here.
+admin.site.register(ImportBatch)
+admin.site.register(Anomaly)
--- transactions_app/admin.py
+++ transactions_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Transaction, TransactionParticipant
 
-# Register your models here.
+admin.site.register(Transaction)
+admin.site.register(TransactionParticipant)
